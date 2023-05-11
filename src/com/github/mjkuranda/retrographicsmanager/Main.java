7ee61bbccf1acccbd41d3ecc97101f6d04fe64f0
mjkuranda <kuranda.marek@gmail.com>
2023-05-11 15:34:35 +0200

    Simple command init

--- src/com/github/mjkuranda/retrographicsmanager/Main.java
+++ src/com/github/mjkuranda/retrographicsmanager/Main.java
@@ -6,8 +6,6 @@ package com.github.mjkuranda.retrographicsmanager;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World, from my app!");
-
         RetroGraphicsManager app = new RetroGraphicsManager();
         app.start();
     }
--- src/com/github/mjkuranda/retrographicsmanager/RetroGraphicsManager.java
+++ src/com/github/mjkuranda/retrographicsmanager/RetroGraphicsManager.java
@@ -5,27 +5,41 @@ import com.github.mjkuranda.retrographicsmanager.palettegenerator.palettes.*;
 import com.github.mjkuranda.retrographicsmanager.palettegenerator.PaletteWriter;
 
 import java.awt.image.BufferedImage;
+import java.util.Scanner;
 
 public class RetroGraphicsManager {
+    public static final String VERSION = "3.0.0";
+    public static final String RELEASE = "2023-05-11";
+
+    private boolean isRunning;
+
+    public RetroGraphicsManager() {
+        isRunning = false;
+    }
 
     public void start() {
-        Palette rectanglePaletteDarker = PaletteFactory.get(PaletteTypes.RECTANGLE, true);
-        Palette rectanglePaletteBrighter = PaletteFactory.get(PaletteTypes.RECTANGLE, false);
-        Palette blockPaletteDarker = PaletteFactory.get(PaletteTypes.BLOCK, true);
-        Palette blockPaletteBrighter = PaletteFactory.get(PaletteTypes.BLOCK, false);
-        PaletteWriter.save(rectanglePaletteDarker, "rect-palette-darker", 4);
-        PaletteWriter.save(rectanglePaletteBrighter, "rect-palette-brighter", 4);
-        PaletteWriter.save(blockPaletteDarker, "block-palette-darker", 4);
-        PaletteWriter.save(blockPaletteBrighter, "block-palette-brighter", 4);
+        isRunning = true;
+        run();
+    }
 
-        BufferedImage retroBrighter = ImageConverter.toRetroImage("retro.png", true);
-        BufferedImage retroDarker = ImageConverter.toRetroImage("retro.png", false);
-        ImageConverter.saveImage(retroBrighter, "retro-brighter.png");
-        ImageConverter.saveImage(retroDarker, "retro-darker.png");
+    public void stop() {
+        isRunning = false;
+    }
 
-        BufferedImage spaceBrighter = ImageConverter.toRetroImage("space.jpg", true);
-        BufferedImage spaceDarker = ImageConverter.toRetroImage("space.jpg", false);
-        ImageConverter.saveImage(spaceBrighter, "space-brighter.png");
-        ImageConverter.saveImage(spaceDarker, "space-darker.png");
+    private void run() {
+        System.out.println("RetroGraphicsManager");
+        System.out.println("Version: \t" + VERSION);
+        System.out.println("Released:\t" + RELEASE);
+        System.out.println("Author:\t\tmjkuranda");
+
+        Scanner scan = new Scanner(System.in);
+        String line;
+
+        while (isRunning) {
+            System.out.print("\n> ");
+
+            line = scan.nextLine();
+            System.out.print(line);
+        }
     }
 }
